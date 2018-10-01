f87304b8d98806483e51500a1a040419ea148638
cbjorklund88 <clara.bjorklund@hotmail.com>
2018-10-01 15:30:27 +0200

    Eslint fix

--- code/src/components/header/index.js
+++ code/src/components/header/index.js
@@ -8,14 +8,14 @@ class Header extends React.Component {
   render() {
     return (
       <header>
-      <label for="hamburger" className="hamburger-label">
+      <label htmlFor="hamburger" className="hamburger-label">
       <div className="hamburger-bar1"></div>
       <div className="hamburger-bar2"></div>
       <div className="hamburger-bar3"></div>
       </label>
       <input id="hamburger"
       className="hamburger-checkbox"
-      type="checkbox"/>
+      type="checkbox" />
         <Grid className="header-grid">
           <a href="http://technigo.io">Technigo</a>
           <a href="http://technigo.io">Boot Camp</a>
