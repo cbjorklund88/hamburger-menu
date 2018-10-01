56232e99c1396a9bd0cdf59bafa562d0ad80f60c
cbjorklund88 <clara.bjorklund@hotmail.com>
2018-10-01 11:53:12 +0200

    Lunchbreak.

--- code/src/components/header/index.js
+++ code/src/components/header/index.js
@@ -8,6 +8,9 @@ class Header extends React.Component {
   render() {
     return (
       <header>
+      <label for="hamburger" className="hamburger-label">
+      </label>
+      <input id="hamburger" className="hamburger-checkbox"type="checkbox"/>
         <Grid className="header-grid">
           <a href="http://technigo.io">Technigo</a>
           <a href="http://technigo.io">Boot Camp</a>
