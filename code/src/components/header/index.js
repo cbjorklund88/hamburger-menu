138a5ce033374c6cf811b33a6ea65fe7ee6755cb
cbjorklund88 <clara.bjorklund@hotmail.com>
2018-10-02 14:57:04 +0200

    hamburger to X when clicked

--- code/src/components/header/index.js
+++ code/src/components/header/index.js
@@ -8,14 +8,14 @@ class Header extends React.Component {
   render() {
     return (
       <header>
-      <label htmlFor="hamburger" className="hamburger-label">
-      <div className="hamburger-bar1"></div>
-      <div className="hamburger-bar2"></div>
-      <div className="hamburger-bar3"></div>
-      </label>
-      <input id="hamburger"
-      className="hamburger-checkbox"
-      type="checkbox" />
+        <input id="hamburger"
+        className="hamburger-checkbox"
+        type="checkbox" />
+        <label for="hamburger" className="hamburger-label">
+          <div className="hamburger-bar1"></div>
+          <div className="hamburger-bar2"></div>
+          <div className="hamburger-bar3"></div>
+        </label>
         <Grid className="header-grid">
           <a href="http://technigo.io">Technigo</a>
           <a href="http://technigo.io">Boot Camp</a>
