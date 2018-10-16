aca1b4711f3ef4df06037e8f5822c34cdca12bf9
cbjorklund88 <clara.bjorklund@hotmail.com>
2018-10-16 09:46:52 +0200

    changed port and fixed the toggle

--- code/src/components/header/index.js
+++ code/src/components/header/index.js
@@ -5,16 +5,30 @@ import "./style.css"
 
 class Header extends React.Component {
 
+  state = {
+    isActive: false
+  }
+  toggleActive = () => {
+    const currentActiveState = this.state.isActive
+    this.setState({ isActive: !currentActiveState })
+  }
+
   render() {
+    // let hamburgerClass = "hamburger"
+    // if(this.state.isActive){
+    //   hamburgerClass = "hamburger active"
+    // }  "ändra hamburger active to : hamburgerClass"
+
     return (
       <header>
-        <input id="hamburger"
-        className="hamburger-checkbox"
-        type="checkbox" />
-        <label for="hamburger" className="hamburger-label">
-          <div className="hamburger-bar1"></div>
-          <div className="hamburger-bar2"></div>
-          <div className="hamburger-bar3"></div>
+        <input
+          id="hamburger"
+          className="hamburger-checkbox"
+          type="checkbox" />
+        <label htmlFor="hamburger" className="hamburger-label"> //change label input to : "hamburger active", remove className and the input. Also change label to: div className "" onClick={}>
+          <div className="hamburger-bar1" />    //add in onClick function {this.toggleActive}
+          <div className="hamburger-bar2" />
+          <div className="hamburger-bar3" />
         </label>
         <Grid className="header-grid">
           <a href="http://technigo.io">Technigo</a>
